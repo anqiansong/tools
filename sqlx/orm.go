1f6c88702e3fe91e5c1441148a8d733fa81b76cd
anqiansong <anqiansong@xiaoheiban.cn>
2021-03-17 10:08:40 +0800

    Initial commit

---
+++ sqlx/orm.go
@@ -0,0 +1,238 @@
+package sqlx
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+var (
+	errInvalidPointer = errors.New("invalid pointer")
+	errNotSettable    = errors.New("can not settable")
+	ErrNoRows         = sql.ErrNoRows
+)
+
+func unmarshalRow(rows *sql.Rows, v interface{}) error {
+	if !rows.Next() {
+		if err := rows.Err(); err != nil {
+			return err
+		}
+		return ErrNoRows
+	}
+
+	if err := must(v); err != nil {
+		return err
+	}
+
+	t := reflect.TypeOf(v)
+	it := indirect(t)
+	switch it.Kind() {
+	case reflect.Bool, reflect.Int, reflect.Int8,
+		reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Float32,
+		reflect.Float64, reflect.String:
+		return scanBasicRow(rows, v)
+	case reflect.Struct:
+		return scanStructRow(rows, v)
+	default:
+		return errors.New("unsupported type")
+	}
+}
+
+func unmarshalRows(rows *sql.Rows, v interface{}) error {
+	if err := must(v); err != nil {
+		return err
+	}
+
+	// ptr *[]int
+	ptr := reflect.TypeOf(v)
+	ptrv := reflect.ValueOf(v)
+	// slice []int
+	slice := ptr.Elem()
+	slicev := reflect.Indirect(ptrv)
+	if !slicev.CanSet() {
+		return errNotSettable
+	}
+
+	item := slice.Elem()
+	itemBaseType := indirect(item)
+	switch itemBaseType.Kind() {
+	case reflect.Bool, reflect.Int, reflect.Int8,
+		reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Float32,
+		reflect.Float64, reflect.String:
+		for rows.Next() {
+			value := reflect.New(itemBaseType)
+			err := rows.Scan(value.Interface())
+			if err != nil {
+				return err
+			}
+
+			if item.Kind() == reflect.Ptr {
+				slicev.Set(reflect.Append(slicev, value))
+			} else {
+				slicev.Set(reflect.Append(slicev, reflect.Indirect(value)))
+			}
+		}
+	case reflect.Struct:
+		columns, err := rows.Columns()
+		if err != nil {
+			return err
+		}
+
+		for rows.Next() {
+			value := reflect.New(itemBaseType)
+			list, err := convertStructFieldsIntoInterfaceSlice(value, columns)
+			if err != nil {
+				return err
+			}
+
+			err = rows.Scan(list...)
+			if err != nil {
+				return err
+			}
+
+			if item.Kind() == reflect.Ptr {
+				slicev.Set(reflect.Append(slicev, value))
+			} else {
+				slicev.Set(reflect.Append(slicev, reflect.Indirect(value)))
+			}
+		}
+	default:
+		return errors.New("unsupported type")
+	}
+
+	return nil
+}
+
+func scanStructRow(rows *sql.Rows, v interface{}) error {
+	columns, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+
+	value := reflect.ValueOf(v)
+	list, err := convertStructFieldsIntoInterfaceSlice(value, columns)
+	if err != nil {
+		return err
+	}
+
+	return rows.Scan(list...)
+}
+
+func convertStructFieldsIntoInterfaceSlice(v reflect.Value, columns []string) ([]interface{}, error) {
+	fields, err := getFields(v)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(columns) < len(fields) {
+		return nil, fmt.Errorf("expected column num %d, but found %d", len(columns), len(fields))
+	}
+
+	list := make([]interface{}, 0)
+	for _, column := range columns {
+		if v, ok := fields[column]; ok {
+			list = append(list, v.Interface())
+		} else {
+			var anonymous interface{}
+			list = append(list, &anonymous)
+		}
+	}
+
+	return list, nil
+}
+
+func getFields(v reflect.Value) (map[string]reflect.Value, error) {
+	ve := reflect.Indirect(v)
+	vt := indirect(ve.Type())
+	fields := make(map[string]reflect.Value)
+	for i := 0; i < ve.NumField(); i++ {
+		fv := ve.Field(i)
+		ft := vt.Field(i)
+		tag := getTag(ft)
+
+		fvt := indirect(fv.Type())
+		if fv.Kind() == reflect.Ptr && fv.IsNil() {
+			if !fv.CanInterface() {
+				return nil, errNotSettable
+			}
+
+			nfv := reflect.New(indirect(fv.Type()))
+			fv.Set(nfv)
+			fvt = indirect(fvt)
+		}
+
+		if fvt.Kind() == reflect.Struct && ft.Anonymous {
+			fds, err := getFields(fv)
+			if err != nil {
+				return nil, err
+			}
+
+			for k, v := range fds {
+				fields[k] = v
+			}
+		} else {
+			if !fv.CanAddr() || !fv.Addr().CanInterface() {
+				return nil, errNotSettable
+			}
+
+			fields[tag] = fv.Addr()
+		}
+	}
+
+	return fields, nil
+}
+
+func getTag(f reflect.StructField) string {
+	tag, ok := f.Tag.Lookup("db")
+	if !ok {
+		tag = f.Name
+	} else {
+		index := strings.Index(tag, ",")
+		if index > 0 {
+			tag = tag[:index]
+		}
+	}
+
+	return tag
+}
+func scanBasicRow(rows *sql.Rows, v interface{}) error {
+	value := reflect.ValueOf(v)
+	elem := reflect.Indirect(value)
+	if !elem.CanSet() {
+		return errNotSettable
+	}
+
+	return rows.Scan(value.Interface())
+}
+
+func indirect(t reflect.Type) reflect.Type {
+	if t.Kind() == reflect.Ptr {
+		return t.Elem()
+	}
+
+	return t
+}
+
+func must(v interface{}) error {
+	t := reflect.TypeOf(v)
+	if t.Kind() != reflect.Ptr {
+		return errInvalidPointer
+	}
+
+	value := reflect.ValueOf(v)
+	if value.IsNil() {
+		return errInvalidPointer
+	}
+
+	if !value.IsValid() || !value.CanInterface() {
+		return errInvalidPointer
+	}
+
+	return nil
+}
---
+++ sqlx/orm_test.go
@@ -0,0 +1,165 @@
+package sqlx
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOrm(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+
+	rs := mock.NewRows([]string{"id"}).FromCSVString("1")
+	t.Run("basic", func(t *testing.T) {
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+		row, err := db.Query("select id from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		var data int
+		err = unmarshalRow(row, &data)
+		assert.Nil(t, err)
+		assert.Equal(t, 1, data)
+	})
+
+	t.Run("struct", func(t *testing.T) {
+		rs := mock.NewRows([]string{"id", "name", "Age"}).FromCSVString("1,test,20")
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+		type Foo struct {
+			Id   int64  `db:"id"`
+			Name string `db:"name"`
+			Age  int
+		}
+		var foo Foo
+		rows, err := db.Query("select id,name,Age from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		err = unmarshalRow(rows, &foo)
+		assert.Nil(t, err)
+		assert.True(t, foo.Id == 1 && foo.Name == "test" && foo.Age == 20)
+	})
+
+	t.Run("struct", func(t *testing.T) {
+		rs := mock.NewRows([]string{"id", "name", "age"}).FromCSVString("1,test,20")
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+		type Foo struct {
+			Id   int64  `db:"id"`
+			Name string `db:"name"`
+			Age  int
+		}
+		var foo Foo
+		rows, err := db.Query("select id,name,age from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		err = unmarshalRow(rows, &foo)
+		assert.Nil(t, err)
+		assert.True(t, foo.Id == 1 && foo.Name == "test" && foo.Age == 0)
+	})
+
+	t.Run("struct anonymous", func(t *testing.T) {
+		rs := mock.NewRows([]string{"id", "name", "Age", "id_number", "gender", "nickname"}).FromCSVString("1,test,20,1001,男,test_alias")
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+		type Bar struct {
+			IdNumber string `db:"id_number"`
+			Gender   string `db:"gender"`
+		}
+
+		type Foo struct {
+			Id   int64  `db:"id"`
+			Name string `db:"name"`
+			Age  int
+			Bar
+		}
+
+		var foo Foo
+		rows, err := db.Query("select id,name,age from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		err = unmarshalRow(rows, &foo)
+		assert.Nil(t, err)
+		assert.True(t, foo.Id == 1 && foo.Name == "test" && foo.Age == 20 && foo.IdNumber == "1001" && foo.Gender == "男")
+	})
+
+	t.Run("slice pointer base", func(t *testing.T) {
+		rs := mock.NewRows([]string{"id"}).FromCSVString("1\n2")
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+
+		var foo []*int
+		rows, err := db.Query("select id from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		err = unmarshalRows(rows, &foo)
+		assert.Nil(t, err)
+		var ret []int
+		for _, i := range foo {
+			ret = append(ret, *i)
+		}
+		assert.Equal(t, []int{1, 2}, ret)
+	})
+
+	t.Run("slice base", func(t *testing.T) {
+		rs := mock.NewRows([]string{"id"}).FromCSVString("1\n2")
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+
+		var foo []int
+		rows, err := db.Query("select id from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		err = unmarshalRows(rows, &foo)
+		assert.Nil(t, err)
+		assert.Equal(t, []int{1, 2}, foo)
+	})
+
+	t.Run("slice struct", func(t *testing.T) {
+		rs := mock.NewRows([]string{"id", "name"}).FromCSVString("1,test1\n2,test2")
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+
+		type Foo struct {
+			Id   int64  `db:"id"`
+			Name string `db:"name"`
+		}
+		var foo []*Foo
+		rows, err := db.Query("select id from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		err = unmarshalRows(rows, &foo)
+		assert.Nil(t, err)
+		assert.Equal(t, []*Foo{
+			{
+				Id:   1,
+				Name: "test1",
+			},
+			{
+				Id:   2,
+				Name: "test2",
+			},
+		}, foo)
+	})
+
+	t.Run("slice pointer struct", func(t *testing.T) {
+		rs := mock.NewRows([]string{"id", "name"}).FromCSVString("1,test1\n2,test2")
+		mock.ExpectQuery("select (.+) from user where id = ?").WithArgs(1).WillReturnRows(rs)
+
+		type Foo struct {
+			Id   int64  `db:"id"`
+			Name string `db:"name"`
+		}
+		var foo []*Foo
+		rows, err := db.Query("select id from user where id = ?", 1)
+		assert.Nil(t, err)
+
+		err = unmarshalRows(rows, &foo)
+		assert.Nil(t, err)
+		assert.Equal(t, []*Foo{
+			{
+				Id:   1,
+				Name: "test1",
+			},
+			{
+				Id:   2,
+				Name: "test2",
+			},
+		}, foo)
+	})
+}
